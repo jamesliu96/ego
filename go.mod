8a48b1982802072efdfa7aae68c7505c76fb5f2e
James Liu <jamesliu96@users.noreply.github.com>
2024-02-06 16:09:41 +0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jamesliu96/promise
+
+go 1.21
---
+++ promise_js.go
@@ -0,0 +1,31 @@
+//go:build js && wasm
+
+package promise
+
+import (
+	"fmt"
+	"syscall/js"
+)
+
+// PromiseOf returns an async function to be used by JavaScript.
+func PromiseOf(fn func(this js.Value, args []js.Value) any) js.Func {
+	return js.FuncOf(func(this js.Value, args []js.Value) any {
+		return js.Global().Get("Promise").New(js.FuncOf(func(_ js.Value, _args []js.Value) any {
+			resolve, reject := _args[0], _args[1]
+			go func() {
+				defer func() {
+					if r := recover(); r != nil {
+						reject.Invoke(js.Global().Get("Error").New(fmt.Sprintf("%+v", r)))
+					}
+				}()
+				resolve.Invoke(fn(this, args))
+			}()
+			return nil
+		}))
+	})
+}
+
+// Keep thread alive
+func KeepAlive() {
+	<-make(chan struct{})
+}
